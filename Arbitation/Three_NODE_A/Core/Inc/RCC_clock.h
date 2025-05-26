f43d862d4fd32d247c76ae10b12e7365f5f3cdeb
SpBelt <120802620+PakapolPolpinich@users.noreply.github.com>
2025-05-26 08:38:08 +0700

    first

---
+++ Arbitation/Three_NODE_A/Core/Inc/RCC_clock.h
@@ -0,0 +1,16 @@
+/*
+ * RCC_clock.h
+ *
+ *  Created on: May 18, 2025
+ *      Author: Admin
+ */
+
+#ifndef INC_RCC_CLOCK_H_
+#define INC_RCC_CLOCK_H_
+
+
+#include "stm32h5xx_hal.h"
+
+void SystemClock_Config(void);
+
+#endif /* INC_RCC_CLOCK_H_ */
